5aa9a089af8d11422757063cd846e1a568d86ed3
Divya Nagarajan <DivyaNaga@Divyas-MacBook-Pro.local>
2023-03-28 14:15:02 -0400

    Changed variable names

--- week-5/src/gtf_DESeq.R
+++ week-5/src/gtf_DESeq.R
@@ -23,14 +23,14 @@ gtf_type <- data.frame(type = as.character(gtf@elementMetadata@listData[["type"]
 gtf_type <- dplyr::filter(gtf_type, type == "transcript")
 
 # gene mapping FOR SEDM vs SEEM analysis
-week2_res <- read.csv(SEDM_vs_SEEM_input)
-gene_mapping_week2 <- data.frame(Transcript_id = week2_res[["X"]])
+SEDM_vs_SEEM_res <- read.csv(SEDM_vs_SEEM_input)
+gene_mapping_week2 <- data.frame(Transcript_id = SEDM_vs_SEEM_res[["X"]])
 gene_mapping_SEDM_vs_SEEM <- merge(gene_mapping_week2, gtf_type, by = "Transcript_id")
 gene_mapping_SEDM_vs_SEEM <- gene_mapping_SEDM_vs_SEEM[c("Transcript_id","gene_name")]
 
 #gene names A687_24hr vs Nalm6_0hr
-week4_res <- read.csv(A687_vs_Nalm6_input)
-gene_names_A687_24hr_vs_Nalm6_0hr <- data.frame(gene_name = week4_res[["X"]])
+A687_24hr_vs_Nalm6_0hr_res <- read.csv(A687_vs_Nalm6_input)
+gene_names_A687_24hr_vs_Nalm6_0hr <- data.frame(gene_name = A687_24hr_vs_Nalm6_0hr_res[["X"]])
 
 #Venn Diagram
 png(filename=Venn_Out) #output graph
